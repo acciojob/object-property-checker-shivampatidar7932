ebe11e5889da860e2cec544ebcf484af254aea58
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-21 13:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+    // Check if the global object sampleObject contains the given key
+    return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
 
 // Do not change the code below
